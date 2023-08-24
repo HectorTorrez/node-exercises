37e8954850161817417ff77b383a3a2654d5a071
Hector <torrezhectorb@gmail.com>
2023-08-24 11:43:10 -0600

    new methods

--- node-exercises/first-express-server/index.js
+++ node-exercises/first-express-server/index.js
@@ -1,11 +1,44 @@
 import express from 'express';
 const app = express()
+const port = 3000
+
+app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('<h1>Héctor Torrez</h1>')
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>Info</h1>')
+})
+app.post( "/register", (req, res) => {
+    res.sendStatus(201)
+})
+
+app.put( "/user/hector", (req, res) => {
+    res.sendStatus(200)
+})
+app.delete( "/user/hector", (req, res) => {
+    res.sendStatus(200)
+})
+
+app.use('/*', (req, res)=>{
+    res.send('<h1>404</h1>')
+
+})
 
 
 
+// const server = app.listen(0, ()=>{
+//     console.log(`Server running in http://localhost:${server.address().port}`)
+// })
 
-const server = app.listen(0, ()=>{
-    console.log(`Server running in http://localhost:${server.address().port}`)
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`)
 })
 
 
