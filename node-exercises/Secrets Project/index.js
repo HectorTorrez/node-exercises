5b6f41d78bf0eea5f01f75d82ab9524ae5012caf
Hector <torrezhectorb@gmail.com>
2023-08-26 17:29:42 -0600

    secret project

--- node-exercises/Secrets Project/index.js
+++ node-exercises/Secrets Project/index.js
@@ -1,29 +1,47 @@
+//The password is ILoveProgramming
 import express from 'express'
 import bodyParser from 'body-parser'
 import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { bandNameGenerator, loggin, banName } from './myMiddleware.js'
 const __dirname = dirname(fileURLToPath(import.meta.url))
+
+
 
 const app = express()
 const port = 3000
 
+let userIsAuthorised = false
 
 app.use(bodyParser.urlencoded({ extended: true }))
+
+const loggin = ((req, res, next) => {
+    if(req.body['password'] === 'ILoveProgramming'){
+        userIsAuthorised = true
+    }
+    
+    next()
+})
+app.use(loggin)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
-app.use(loggin)
-
-app.use(bandNameGenerator)
 
 
-app.post('/submit', (req, res) => {
-    console.log(req.body)
-    res.send(`<h1>${banName}</h1>`)
+
+app.post('/check', (req, res) => {
+    if(userIsAuthorised){
+        res.sendFile(__dirname + '/public/secret.html')
+    }else{
+        res.sendFile(__dirname + '/public/index.html')
+    }
+
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+
